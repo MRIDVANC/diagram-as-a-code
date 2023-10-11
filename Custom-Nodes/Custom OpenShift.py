4af0333324336cab97c61ac7dca2e4c2fb57a002
MRIDVANC <24781973+MRIDVANC@users.noreply.github.com>
2023-10-12 00:48:39 +0300

    OpenShift Custom Template TEst 1

--- Custom-Nodes/Custom OpenShift.py
+++ Custom-Nodes/Custom OpenShift.py
@@ -1,20 +1,21 @@
 from diagrams import Diagram, Cluster
 from diagrams.custom import Custom
+from urllib.request import urlretrieve
+
+with Diagram("Custom with remote icons", show=False, filename="custom_remote", direction="LR"):
+
+  # download the icon image file
+  diagrams_url = "https://github.com/mingrammer/diagrams/raw/master/assets/img/diagrams.png"
+  diagrams_icon = "diagrams.png"
+  urlretrieve(diagrams_url, diagrams_icon)
+
+  diagrams = Custom("Diagrams", diagrams_icon)
+
+  with Cluster("Some Providers"):
+      openshift_url ="https://github.com/MRIDVANC/diagram-as-a-code/blob/master/Custom-Nodes/openshift-logo.png"
+      openshift_icon ="openshift-logo.png"
+
+      openshift = Custom("openshift", openshift_icon)
 
 
-with Diagram("Custom with local icons\n Can be downloaded here: \nhttps://creativecommons.org/about/downloads/", show=False, filename="custom_local", direction="LR"):
-  cc_heart = Custom("Creative Commons", "./my_resources/cc_heart.black.png")
-  cc_attribution = Custom("Credit must be given to the creator", "./my_resources/cc_attribution.png")
-
-  cc_sa = Custom("Adaptations must be shared\n under the same terms", "./my_resources/cc_sa.png")
-  cc_nd = Custom("No derivatives or adaptations\n of the work are permitted", "./my_resources/cc_nd.png")
-  cc_zero = Custom("Public Domain Dedication", "./my_resources/cc_zero.png")
-
-  with Cluster("Non Commercial"):
-    non_commercial = [Custom("Y", "./my_resources/cc_nc-jp.png") - Custom("E", "./my_resources/cc_nc-eu.png") - Custom("S", "./my_resources/cc_nc.png")]
-
-  cc_heart >> cc_attribution
-  cc_heart >> non_commercial
-  cc_heart >> cc_sa
-  cc_heart >> cc_nd
-  cc_heart >> cc_zero
+  diagrams >> openshift
