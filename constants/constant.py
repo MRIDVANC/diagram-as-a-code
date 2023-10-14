0cf67dd40a1ec074f91be2a303e973268ae42b79
MRIDVANC <24781973+MRIDVANC@users.noreply.github.com>
2023-10-14 22:52:01 +0300

    Colour and icon tests

---
+++ constants/constant.py
@@ -0,0 +1,31 @@
+# constants.py
+#
+# # Web sitesinin URL'si
+# WEBSITE_URL = "https://www.example.com"
+#
+# # Renkler
+# PRIMARY_COLOR = "#336699"
+# SECONDARY_COLOR = "#FF9900"
+#
+# # Sayfa başlıkları
+# HOME_TITLE = "Ana Sayfa"
+# ABOUT_TITLE = "Hakkımızda"
+# CONTACT_TITLE = "İletişim"
+# başka_bir_dosya.py
+#
+# import constant
+#
+# print("Web Sitesi URL'si:", constants.WEBSITE_URL)
+# print("Ana Sayfa Başlığı:", constants.HOME_TITLE)
+
+
+
+'''ANSIBLE'''
+
+ANSIBLE_LOGO = "https://raw.githubusercontent.com/MRIDVANC/diagram-as-a-code/master/custom_icons/ansible/ansible.png"
+
+
+'''REDHAT'''
+
+
+OPENSHIFT_LOGO ="https://github.com/MRIDVANC/diagram-as-a-code/blob/master/custom_icons/redhat/openshift/openshift.png"
