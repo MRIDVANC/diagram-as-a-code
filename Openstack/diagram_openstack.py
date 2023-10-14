99f260127ecbdd8c9182c17dc02bbb032d1b7e45
MRIDVANC <24781973+MRIDVANC@users.noreply.github.com>
2023-10-14 23:07:17 +0300

    Colour and icon tests

--- Openstack/diagram_openstack.py
+++ Openstack/diagram_openstack.py
@@ -7,6 +7,7 @@ from diagrams.openstack.storage import Cinder
 from diagrams.oci.compute import BareMetal, BM
 from diagrams.oci.connectivity import Backbone
 from customs.ansible import Ansible
+from customs.openshift import Openshift
 
 with (Diagram("Openstack Cloud", show=False, direction="TB")):
     # Datacenter1
@@ -15,6 +16,7 @@ with (Diagram("Openstack Cloud", show=False, direction="TB")):
     server1: BM = BareMetal("CTR01")
     server2: BM = BareMetal("CTR02")
     Ansible = Ansible()
+    Openshift = Openshift()
 
     with Cluster("CONTROL GROUP"):
         ctrsrv =  Ansible >> [ server1, server2 ]
