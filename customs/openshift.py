466df7d9eb270464dafe32a4df10ba8fad490fa1
MRIDVANC <24781973+MRIDVANC@users.noreply.github.com>
2023-10-14 23:20:50 +0300

    Colour and icon tests

--- customs/openshift.py
+++ customs/openshift.py
@@ -1,15 +1,13 @@
 from diagrams.custom import Custom
 from urllib.request import urlretrieve
-
 from constants.constant import OPENSHIFT_LOGO
 
 
 class Openshift(Custom):
-
-  def __init__(self):
+  def __init__(self,label):
     _url = OPENSHIFT_LOGO
     _icon = "openshift.png"
-    _label = "openshit"
+    _label = label
     urlretrieve(_url, _icon)
     super().__init__(_label, _icon)
 
