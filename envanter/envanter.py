d6cd3bea229e38fc823c9beb098faec51c673015
MRIDVANC <24781973+MRIDVANC@users.noreply.github.com>
2023-10-18 10:38:26 +0300

    Colour and icon tests

--- envanter/envanter.py
+++ envanter/envanter.py
@@ -23,3 +23,31 @@ print(second)
 
 print("Third Column:")
 print(third)
+
+excel_file = "envanter.xlsx"
+
+# Excel dosyasını oku
+df = pd.read_excel(excel_file)
+
+# Her satırı temsil edecek bir sınıf tanımlayalım
+class Switch:
+    def __init__(self, switch_name, port, connected_server):
+        self.switch_name = switch_name
+        self.port = port
+        self.connected_server = connected_server
+
+# Sınıf nesnelerini oluşturup bir listeye ekleyelim
+switch_objects = []
+for index, row in df.iterrows():
+    switch_name = row["Switch isimleri"]
+    port = row["1. Port"]
+    connected_server = row["Takılı olduğu sunucu"]
+    switch = Switch(switch_name, port, connected_server)
+    switch_objects.append(switch)
+
+# Sınıf nesnelerini kullanabilirsiniz
+for switch in switch_objects:
+    print("Switch Name:", switch.switch_name)
+    print("Port:", switch.port)
+    print("Connected Server:", switch.connected_server)
+    print()
