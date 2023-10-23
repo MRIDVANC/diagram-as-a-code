e1bf1bc761af0f166502189887ca19bf15a7a330
MRIDVANC <24781973+MRIDVANC@users.noreply.github.com>
2023-10-23 10:11:22 +0300

    README.md update

--- Openstack/diagram_openstack.py
+++ Openstack/diagram_openstack.py
@@ -1,14 +1,8 @@
-from typing import List
-
 from diagrams import Diagram, Cluster
-from diagrams.openstack.networking import Neutron
-from diagrams.openstack.storage import Cinder
 from diagrams.oci.compute import BareMetal, BM
 from diagrams.oci.connectivity import Backbone
+
 from customs.ansible import Ansible
-
-
-
 
 with (Diagram("Openstack Cloud", show=False, direction="TB")):
     # Datacenter1
@@ -17,6 +11,7 @@ with (Diagram("Openstack Cloud", show=False, direction="TB")):
     server1: BM = BareMetal("CTR01")
     server2: BM = BareMetal("CTR02")
     Ansible = Ansible()
+
 
 
     with Cluster("CONTROL GROUP"):
